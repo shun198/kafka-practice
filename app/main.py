c7bde44231b939908b6a5878265580ac66851168
shun198 <shunhiroseluvmri@gmail.com>
2025-06-01 09:50:44 +0900

    include batch and app for kafka

---
+++ app/main.py
@@ -0,0 +1,45 @@
+from confluent_kafka import Consumer
+import json
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+conf = {
+    'bootstrap.servers': 'localhost:9092',
+    'group.id': 'fastapi-consumer-group',
+    'auto.offset.reset': 'earliest'
+}
+
+# https://docs.confluent.io/kafka-clients/python/current/overview.html#ak-consumer
+def consume_messages():
+    consumer = Consumer(conf)
+    consumer.subscribe(['my-topic'])
+    try:
+        while True:
+            msg = consumer.poll(1.0)
+            if msg is None:
+                logging.info("No message")
+                continue
+            if msg.error():
+                print(f"Error while consuming messages: {msg.error()}")
+                logging.error(msg.error())
+            data = json.loads(msg.value().decode('utf-8'))
+            print(f"message: {data}")
+            logging.info(data)
+    finally:
+        consumer.close()
+        logging.info("Consumer closed")
+
+
+def startup():
+    logging.info("Starting consumer...")
+    consume_messages()
+
+# https://stackoverflow.com/questions/75839415/kafka-fastapi-docker-template
+if __name__ == "__main__":
+    try:
+        startup()
+    except Exception as e:
+        print(f"Exception occurred: {e}")
---
+++ batch/main.py
@@ -0,0 +1,16 @@
+from confluent_kafka import Producer
+import json
+
+# https://docs.confluent.io/kafka-clients/python/current/overview.html#ak-producer
+conf = {'bootstrap.servers': 'localhost:9092'}
+producer = Producer(conf)
+
+def send_messages(err, msg):
+    if err is not None:
+        print(f"配信失敗: {err}")
+    else:
+        print(f"メッセージ配信成功: {msg.value().decode('utf-8')}")
+
+message = {"event": "task_done", "id": 123}
+producer.produce(topic="my-topic", value=json.dumps(message), callback=send_messages)
+producer.flush()
